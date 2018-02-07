c53cfef972fa386ff2b467aee3fe4352d828611f
Alessandro Vinaccia <vinacciaalessandro@gmail.com>
2018-02-07 18:54:17 +0100

    second commit

---
+++ prova.js
@@ -0,0 +1,4 @@
+var glossary = {
+    "dog" : {trad : "cane", "audio" : new Audio("./assets/sounds/dog.wav"), img: "./assets/images/dog.jpeg"},
+    "cat" : {trad : "gatto", "audio" : new audio("./assets/sounds/cat.wav"), img: "./assets/images/dog.jpeg"},
+}
